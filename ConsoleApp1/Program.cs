34dcebed01121893e1379c2f2c93b50841f4b713
Filip Pavlas <pavlas@intens.cz>
2024-08-02 09:17:04 +0200

    Exception when non-json metadata is sent and open telemetry is active

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,97 +1,103 @@
-﻿namespace ConsoleApp1
+﻿using System.Diagnostics;
+using OpenTelemetry;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+
+namespace ConsoleApp1
 {
     using System;
-    using System.Diagnostics;
     using System.Text.Json;
     using EventStore.Client;
 
     internal class Program
     {
-        static async Task Main(string[] args){
-            AppDomain.CurrentDomain.FirstChanceException += (sender, eventArgs) => { Console.WriteLine($"[FirstChance] - {eventArgs.Exception.ToString()} - sender is {sender?.GetType()}"); };
+        static async Task Main(string[] args)
+        {
+            using var tracerProvider = Sdk.CreateTracerProviderBuilder()
+                .SetResourceBuilder(ResourceBuilder.CreateDefault().AddService("MySample"))
+                .AddSource("eventstoredb")
+                .AddConsoleExporter()
+                .Build();
+            var listener = new DiagnosticListener("eventstoredb");
 
-            const String connectionString = "esdb://admin:changeit@192.168.10.90:2113?tls=false&tlsVerifyCert=false";
+            const String connectionString = "esdb://admin:changeit@localhost:2113?tls=false&tlsVerifyCert=false";
             var settings = EventStoreClientSettings.Create(connectionString);
             var client = new EventStoreClient(settings);
 
-            String streamName = $"some-stream";
-            String groupName = $"subscription-group";
+            String streamName = $"test-stream-0";
 
-            var evt = new{
-                             EntityId = Guid.NewGuid().ToString("N"),
-                             ImportantData = "I wrote my first event!"
-                         };
+            var failingMetadata = new ReadOnlyMemory<byte>("\n\u0018OrganizationCreatedEvent"u8.ToArray());
+            var okMetadata = JsonSerializer.SerializeToUtf8Bytes(new Dictionary<string, string>() { { "test", "test" } });
 
+            var metadata = failingMetadata;
 
-            IDictionary<String, String> metadata = new Dictionary<String, String>();
-
-            ActivityTraceId activityId = ActivityTraceId.CreateRandom();
-            ActivitySpanId activitySpanId = ActivitySpanId.CreateRandom();
-
-            metadata.Add("$traceId", activityId.ToHexString());
-            metadata.Add("$spanId", activitySpanId.ToHexString());
-
-            EventData eventData = new(
-                                      Uuid.NewUuid(),
-                                      "TestEvent",
-                                      JsonSerializer.SerializeToUtf8Bytes(evt),
-                                      JsonSerializer.SerializeToUtf8Bytes(metadata)
-                                     );
-
-            await client.AppendToStreamAsync(
-                                             streamName,
-                                             StreamState.Any,
-                                             new[] { eventData },
-                                             cancellationToken: CancellationToken.None
-                                            );
+            var cancel = new CancellationTokenSource();
+            var producerTask = Task.Run(async () =>
+            {
+                try
+                {
+                    while (true)
+                    {
+                        await Task.Delay(5000, cancel.Token);
+                        Console.WriteLine("===================");
+                        Console.WriteLine("===================");
+                        Console.WriteLine("Sending new event");
+                        EventData eventData = new(Uuid.NewUuid(), "TestEvent", metadata, metadata, "application/octet-stream");
+                        await client.AppendToStreamAsync(
+                                                         streamName,
+                                                         StreamState.Any,
+                                                         new[] { eventData },
+                                                         cancellationToken: CancellationToken.None
+                                                        );
+                    }
+                }
+                catch (TaskCanceledException e)
+                {
+                }
+            }, cancel.Token);
 
             UserCredentials userCredentials = new("admin", "changeit");
-            EventStorePersistentSubscriptionsClient persistentSubscriptionsClient = new(EventStoreClientSettings.Create(connectionString));
-
-            PersistentSubscriptionSettings persistentSubscriptionSettings = new(true, 
-                                                                                new StreamPosition(0), 
-                                                                                maxRetryCount: 0);
 
             try
             {
-                await persistentSubscriptionsClient.DeleteToStreamAsync(streamName, groupName);
+                var subscription = client.SubscribeToStream(streamName, FromStream.Start, true, userCredentials, CancellationToken.None);
+                await foreach (var message in subscription.Messages.WithCancellation(cancel.Token))
+                {
+                    switch (message)
+                    {
+                        case StreamMessage.SubscriptionConfirmation(var id):
+                            Console.WriteLine($"Confirmed {id}");
+                            break;
+                        case StreamMessage.Event(var evnt):
+                            await EventAppeared(evnt);
+                            break;
+                        case StreamMessage.CaughtUp:
+                            Console.WriteLine("Processing events live");
+                            break;
+                        case StreamMessage.FellBehind:
+                            Console.WriteLine("Catching up");
+                            break;
+                    }
+                }
             }
-            catch(Exception e){
-                Console.WriteLine(e.Message);
+            catch (Exception e)
+            {
+                Console.WriteLine("ERROR OCCURED-----------------------");
+                Console.WriteLine(e.ToString());
+                Console.WriteLine("------------------------------------");
             }
 
-            await persistentSubscriptionsClient.CreateToStreamAsync(
-                                                                    streamName,
-                                                                    groupName,
-                                                                    persistentSubscriptionSettings,
-                                                                    userCredentials:userCredentials
-                                                                   );
-
-            await Task.Delay(TimeSpan.FromSeconds(1));
-
-            await persistentSubscriptionsClient.SubscribeToStreamAsync(streamName,
-                                                                       groupName,
-                                                                       EventAppeared,
-                                                                       SubscriptionDropped,
-                                                                       userCredentials,
-                                                                       10,
-                                                                       CancellationToken.None);
+            await cancel.CancelAsync();
+            listener.Dispose();
+            await producerTask;
 
             Console.ReadKey();
         }
 
-        private static void SubscriptionDropped(PersistentSubscription arg1, SubscriptionDroppedReason arg2, Exception? arg3){
-            Console.WriteLine($"{arg2}");
-        }
-
-        private static Task EventAppeared(PersistentSubscription arg1, ResolvedEvent arg2, Int32? arg3, CancellationToken arg4){
-            Console.WriteLine($"Event Appeared from {arg2.OriginalStreamId}");
-
-            arg1.Ack(arg2);
-
+        private static Task EventAppeared(ResolvedEvent @event)
+        {
+            Console.WriteLine($"Event Appeared from {@event.OriginalStreamId}");
             return Task.CompletedTask;
         }
     }
-
-
 }
